5cc76b9f91877dad9e8eb6ef99ac48c9dfd955cf
Nathally Neves <Nathally0610@unesc.net>
2025-07-23 16:53:32 -0300

    Mais algumas alterações

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import Navbar from "./components/Navbar/Navbar";
-import Intro from "./components/Intro/intro";
+import Intro from "./components/Intro/Intro";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
